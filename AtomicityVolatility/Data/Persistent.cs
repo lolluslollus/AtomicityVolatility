b579dd835dc0d9bf2c59dc0e4338ad9bdb543ad5
lollus <lolluslollus@hotmail.com>
2016-02-02 21:59:31 +0100

    enhancements + some conclusions

--- AtomicityVolatility/Data/Persistent.cs
+++ AtomicityVolatility/Data/Persistent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Utilz.Data;
 
@@ -9,14 +11,66 @@ namespace AtomicityVolatility.Data
 {
 	public class Persistent : ObservableData
 	{
-		private bool _bool0 = false;
-		public bool Bool0 { get { return _bool0; } set { _bool0 = value; } }
+		private bool _boolObservable = false;
+		public bool BoolObservable { get { return _boolObservable; } set { _boolObservable = value; /*RaisePropertyChanged_UI();*/ } } // this fails misteriously
 
-		private bool _bool1 = false;
-		public bool Bool1 { get { return _bool1; } set { _bool1 = value; } }
+		private bool _boolLocked = false; private readonly object _boolLocker = new object();
+		public bool BoolLocked
+		{
+			get
+			{
+				lock (_boolLocker)
+				{
+					return _boolLocked;
+				}
+			}
+			set
+			{
+				lock (_boolLocker)
+				{
+					_boolLocked = value;
+				}
+			}
+		}
 
-		private bool _bool2 = false;
-		public bool Bool2 { get { return _bool2; } set { _bool2 = value; } }
+		private volatile bool _boolVolatile = false; private readonly object _boolVoLo = new object();
+		public bool BoolVolatile
+		{
+			get { return _boolVolatile; }
+			set
+			{
+				//lock (_boolVoLo)
+				{
+					_boolVolatile = value;
+				}
+			}
+		}
+
+		private bool _boolVolatileRead = false; private readonly object _boolVoLoRe = new object();
+		public bool BoolVolatileRead
+		{
+			get { return Volatile.Read(ref _boolVolatileRead); }
+			set
+			{
+				//lock (_boolVoLoRe)
+				{
+					_boolVolatileRead = value;
+				}
+			}
+		}
+
+		private bool _boolVolatileReadWrite = false; private readonly object _boolVoLoReWr = new object();
+		public bool BoolVolatileReadWrite
+		{
+			get { return Volatile.Read(ref _boolVolatileReadWrite); }
+			set
+			{
+				//lock (_boolVoLoReWr)
+				{
+					Volatile.Write(ref _boolVolatileReadWrite, value);
+				}
+			}
+		}
 
 		private byte _byte0 = 0;
 		public byte Byte0 { get { return _byte0; } set { _byte0 = value; } }
@@ -26,6 +80,16 @@ namespace AtomicityVolatility.Data
 
 		private byte _byte2 = 0;
 		public byte Byte2 { get { return _byte2; } set { _byte2 = value; } }
+
+		public async Task MakeTrueAsync()
+		{
+			await Task.Delay(1000).ConfigureAwait(false);
+			BoolObservable = true;
+			BoolLocked = true;
+			BoolVolatile = true;
+			BoolVolatileRead = true;
+			Debug.WriteLine("MakeTrueAsync() set all to true");
+		}
 
 	}
 }
--- AtomicityVolatility/MainPage.xaml.cs
+++ AtomicityVolatility/MainPage.xaml.cs
@@ -20,7 +20,14 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-// LOLLO NOTE debug this with "optimise code"
+// LOLLO NOTE debug this with "optimise code" and "native tool chain"
+// LOLLO NOTE these examples are based on while() ;
+// it hogs the thread, which never gets a chance to get the new values.
+// if you use 
+// while () await Task.Delay(50);
+// the thread spins a couple of times and then gets updated.
+// Still, this delay can be critical in some cases. After measuring the performance, it looks like the volatile keyword is the most efficient solution.
+
 
 namespace AtomicityVolatility
 {
@@ -29,12 +36,16 @@ namespace AtomicityVolatility
 	/// </summary>
 	public sealed partial class MainPage : ObservablePage
 	{
+		private bool _isGetOut = false;
+
 		private bool _boolOfMain = false;
 		public bool BoolOfMainVolatileRead { get { return Volatile.Read(ref _boolOfMain); } set { _boolOfMain = value; } }
 		public bool BoolOfMainVolatileReadWrite { get { return Volatile.Read(ref _boolOfMain); } set { Volatile.Write(ref _boolOfMain, value); } }
-		public bool BoolOfMain = false;
+		public bool BoolOfMainProperty { get { return _boolOfMain; } set { _boolOfMain = value; } }
+		public bool BoolOfMainObservableProperty { get { return _boolOfMain; } set { _boolOfMain = value; RaisePropertyChanged_UI(); } }
 		public volatile bool BoolOfMainVolatile = false;
 		private static bool BoolOfMainStatic = false;
+
 		private Persistent _persistent = null;
 		public Persistent Persistent { get { return _persistent; } private set { _persistent = value; RaisePropertyChanged_UI(); } }
 
@@ -49,7 +60,7 @@ namespace AtomicityVolatility
 		{
 			MakeBtnRed(sender);
 
-			BoolOfMain = false;
+			BoolOfMainProperty = false;
 			var checker = new StatusCheckerWithRefToMain(this);
 
 			//await checker.UpdateStatus().ConfigureAwait(false); // this one updates and we can see the result here
@@ -57,22 +68,23 @@ namespace AtomicityVolatility
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
 
 
-			while (!BoolOfMain) ;
+			while (!BoolOfMainProperty || _isGetOut) ; // { await Task.Delay(50); }
 
 			MakeBtnGreen(sender);
 		}
+
 		private async void OnButton1_Click(object sender, RoutedEventArgs e)
 		{
 			MakeBtnRed(sender);
 
-			_persistent.Bool0 = false;
+			_persistent.BoolObservable = false;
 			var checker = new StatusCheckerWithRefToPersistent(_persistent);
 
 			//await checker.UpdateStatus().ConfigureAwait(false); // this one updates and we can see the result here
 			//Task www = checker.UpdateStatus(); // this works
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this works
 
-			while (!_persistent.Bool0) ;
+			while (!_persistent.BoolObservable || _isGetOut) ;
 
 			MakeBtnGreen(sender);
 		}
@@ -89,7 +101,7 @@ namespace AtomicityVolatility
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this works
 
 
-			while (!BoolOfMainVolatile) ;
+			while (!BoolOfMainVolatile || _isGetOut) ;
 
 			MakeBtnGreen(sender);
 		}
@@ -105,7 +117,7 @@ namespace AtomicityVolatility
 			//Task www = checker.UpdateStatus(); // this works
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this works
 
-			while (!checker.StatusUpdated) ;
+			while (!checker.StatusUpdated || _isGetOut) ;
 
 			MakeBtnGreen(sender);
 		}
@@ -120,7 +132,7 @@ namespace AtomicityVolatility
 			//Task www = checker.UpdateStatus(); // this one hangs forever
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
 
-			while (!checker.StatusUpdated) ;
+			while (!checker.StatusUpdated || _isGetOut) ;
 
 			MakeBtnGreen(sender);
 		}
@@ -135,7 +147,7 @@ namespace AtomicityVolatility
 			//Task www = checker.UpdateStatus(); // this works
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this works
 
-			while (!checker.StatusUpdated) ;
+			while (!checker.StatusUpdated || _isGetOut) ;
 
 			MakeBtnGreen(sender);
 		}
@@ -151,7 +163,7 @@ namespace AtomicityVolatility
 			//Task www = checker.UpdateStatus(); // this works
 			Task.Run(delegate { return checker.UpdateStatus(); }); // this works
 
-			while (!checker.StatusUpdated) ;
+			while (!checker.StatusUpdated || _isGetOut) ;
 
 			MakeBtnGreen(sender);
 		}
@@ -160,50 +172,201 @@ namespace AtomicityVolatility
 			MakeBtnRed(sender);
 			// this fails sometimes, it's most interesting.
 			BoolOfMainStatic = false;
-			_persistent.Bool0 = false;
-			BoolOfMain = false;
-			BoolOfMainVolatile = false;
-			BoolOfMainVolatileReadWrite = false;
-			BoolOfMainVolatileRead = false;
+			_persistent.BoolObservable = false;
+			
+			_boolOfMain = false;
 
 			Task away = Task.Run(async delegate
 		   {
 			   await Task.Delay(500).ConfigureAwait(false);
 
-			   BoolOfMainStatic = true;
-			   _persistent.Bool0 = true;
-			   BoolOfMain = true;
-			   BoolOfMainVolatile = true;
-			   BoolOfMainVolatileReadWrite = true;
-			   BoolOfMainVolatileRead = true;
+			   //BoolOfMainStatic = true;
+			   //_persistent.Bool0 = true;
+			   //BoolOfMainProperty = true; // fails
+			   //_boolOfMain = true; // fails
+			   //BoolOfMainVolatile = true;
+			   //BoolOfMainVolatileReadWrite = true;
+			   //BoolOfMainVolatileRead = true;
+			   BoolOfMainObservableProperty = true; // fails
 
 			   Debug.WriteLine("updates done");
 		   });
+
+			while (!BoolOfMainObservableProperty || _isGetOut) ; // this hangs forever, and the binding does not pull, either.
 
 			//while (!BoolOfMainStatic) ; // this works
 			//Debugger.Break();
 			//while (!_persistent.Bool0) ; // this works
 			//Debugger.Break();
-			while (!BoolOfMain) ; // this hangs forever; however, 
+
+			//while (!_boolOfMain) ; // this hangs forever; however, 
+			// if true is READ after reading a volatile or static property, then it works, 
+			// never mind if the volatile or static property is set before or after it.
+			// this cannot be reproduced 100%, except that it always hangs when BoolOfMain is written and read alone.
+			// if it is read first, it always hangs.
+
+			//while (!BoolOfMainProperty) ; // this hangs forever ; however, 
 								  // if true is READ after reading a volatile or static property, then it works, 
 								  // never mind if the volatile or static property is set before or after it.
 								  // this cannot be reproduced 100%, except that it always hangs when BoolOfMain is written and read alone.
 								  // if it is read first, it always hangs.
+
+			// it works at home though...
 
 			//Debugger.Break();
 			//while (!BoolOfMainVolatile) ; // this works
 			//Debugger.Break();
 			//while (!BoolOfMainVolatileReadWrite) ; // this works
 			//Debugger.Break();
-			while (!BoolOfMainVolatileRead) ; // this works
+			//while (!BoolOfMainVolatileRead) ; // this works
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton8_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			_persistent.BoolObservable = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			//Task www = checker.UpdateStatus(); // this works
+			Task.Run(delegate { return _persistent.MakeTrueAsync(); }); // this hangs forever
+
+			while (!_persistent.BoolObservable || _isGetOut) ; // { await Task.Delay(50); }
 
 			MakeBtnGreen(sender);
 		}
 
+		private async void OnButton9_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
 
+			_persistent.BoolLocked = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			//Task www = checker.UpdateStatus(); // this works
+			Task.Run(delegate { return _persistent.MakeTrueAsync(); }); // this works
+
+			while (!_persistent.BoolLocked || _isGetOut) ; // await Task.Delay(50);
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton10_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			_persistent.BoolVolatile = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			//Task www = checker.UpdateStatus(); // this works
+			Task.Run(delegate { return _persistent.MakeTrueAsync(); }); // this works
+
+			while (!_persistent.BoolVolatile || _isGetOut) ; // await Task.Delay(50);
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton11_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			_persistent.BoolVolatileRead = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			//Task www = checker.UpdateStatus(); // this works
+			Task.Run(delegate { return _persistent.MakeTrueAsync(); }); // this works
+
+			while (!_persistent.BoolVolatileRead || _isGetOut) ; // await Task.Delay(50);
+
+			MakeBtnGreen(sender);
+		}
+
+		private async void OnButtonPerf_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+			bool test = false;
+			Stopwatch sw = new Stopwatch();
+			int ii = 100000000;
+
+			for (int i = 0; i < ii; i++)
+			{
+				test = _persistent.BoolLocked; test = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolLocked gets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				test = _persistent.BoolObservable; test = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolObservable gets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				test = _persistent.BoolVolatile; test = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolVolatile gets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				test = _persistent.BoolVolatileRead; test = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolVolatileRead gets took " + sw.ElapsedMilliseconds + " msec");
+
+			ii = 10000000;
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				_persistent.BoolLocked = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolLocked sets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				_persistent.BoolObservable = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolObservable sets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				_persistent.BoolVolatile = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolVolatile sets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				_persistent.BoolVolatileRead = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolVolatileRead sets took " + sw.ElapsedMilliseconds + " msec");
+
+			sw.Restart();
+			for (int i = 0; i < ii; i++)
+			{
+				_persistent.BoolVolatileReadWrite = !test;
+			}
+			sw.Stop();
+			Debug.WriteLine("BoolVolatileReadWrite sets took " + sw.ElapsedMilliseconds + " msec");
+
+			MakeBtnGreen(sender);
+		}
+
+		
 		#region utilz
 		private void MakeBtnRed(object sender)
 		{
+			_isGetOut = false;
 			Task ss = RunInUiThreadAsync(delegate
 			{
 				(sender as Button).Foreground = new SolidColorBrush(Colors.Red);
@@ -297,7 +460,7 @@ namespace AtomicityVolatility
 				await Task.Delay(2000).ConfigureAwait(false);
 				Result = "OK";
 				Debug.WriteLine("Status Obtained");
-				_owner.BoolOfMain = true;
+				_owner.BoolOfMainProperty = true;
 			}
 		}
 		class StatusCheckerWithRefToPersistent
@@ -315,7 +478,7 @@ namespace AtomicityVolatility
 				await Task.Delay(2000).ConfigureAwait(false);
 				Result = "OK";
 				Debug.WriteLine("Status Obtained");
-				_owner.Bool0 = true;
+				_owner.BoolObservable = true;
 			}
 		}
 		class StatusCheckerWithRef_VolatileOwner
@@ -337,5 +500,10 @@ namespace AtomicityVolatility
 			}
 		}
 		#endregion utilz
+
+		private void OnGetOut_Click(object sender, RoutedEventArgs e)
+		{
+			_isGetOut = true;
+		}
 	}
 }
