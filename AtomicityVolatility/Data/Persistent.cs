bb30714be60983869c0d8241b508ec58255f45d9
lolluslollus <lolluslollus@hotmail.com>
2016-02-02 17:55:45 +0100

    volatility

---
+++ AtomicityVolatility/Data/Persistent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilz.Data;
+
+namespace AtomicityVolatility.Data
+{
+	public class Persistent : ObservableData
+	{
+		private bool _bool0 = false;
+		public bool Bool0 { get { return _bool0; } set { _bool0 = value; } }
+
+		private bool _bool1 = false;
+		public bool Bool1 { get { return _bool1; } set { _bool1 = value; } }
+
+		private bool _bool2 = false;
+		public bool Bool2 { get { return _bool2; } set { _bool2 = value; } }
+
+		private byte _byte0 = 0;
+		public byte Byte0 { get { return _byte0; } set { _byte0 = value; } }
+
+		private byte _byte1 = 0;
+		public byte Byte1 { get { return _byte1; } set { _byte1 = value; } }
+
+		private byte _byte2 = 0;
+		public byte Byte2 { get { return _byte2; } set { _byte2 = value; } }
+
+	}
+}
---
+++ AtomicityVolatility/MainPage.xaml.cs
@@ -0,0 +1,385 @@
+﻿using AtomicityVolatility.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Utilz.Controlz;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// LOLLO NOTE debug this with "optimise code"
+
+namespace AtomicityVolatility
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : ObservablePage
+	{
+		private bool _boolOfMain = false;
+		public bool BoolOfMainVolatileRead { get { return Volatile.Read(ref _boolOfMain); } set { _boolOfMain= value; } }
+		public bool BoolOfMainVolatileReadWrite { get { return Volatile.Read(ref _boolOfMain); } set { Volatile.Write(ref _boolOfMain, value); } }
+		public bool BoolOfMain = false;
+		public volatile bool BoolOfMainVolatile = false;
+		private static bool BoolOfMainStatic = false;
+		private Persistent _persistent = null;
+		public Persistent Persistent { get { return _persistent; } private set { _persistent = value; RaisePropertyChanged_UI(); } }
+
+
+		public MainPage()
+		{
+			Persistent = new Persistent();
+			InitializeComponent();
+		}
+
+		private async void OnButton0_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			BoolOfMain = false;
+			var checker = new StatusCheckerWithRefToMain(this);
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one updates and we can see the result here
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+
+			while (!BoolOfMain) ;
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton1_Click(object sender, RoutedEventArgs e)
+		{
+			//// this works, it's boring
+			//ResetValues();
+			//UpdateTbs();
+			//await Task.Run(delegate { return Update_Bool0AndBoolStatic_ToTrue_Async(); }).ConfigureAwait(false);
+			//Debug.WriteLine("Bool0 = " + _persistent.Bool0);
+			//Debug.WriteLine("BoolStatic = " + _boolStatic);
+			//UpdateTbs();
+
+			MakeBtnRed(sender);
+
+			_persistent.Bool0 = false;
+			var checker = new StatusCheckerWithRefToPersistent(_persistent);
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one updates and we can see the result here
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+			while (!_persistent.Bool0) ;
+
+			MakeBtnGreen(sender);
+		}
+
+		private async void OnButton2_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			BoolOfMainVolatile = false;
+			var checker = new StatusCheckerWithRef_VolatileOwner(this);
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one updates and we can see the result here
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+
+			while (!BoolOfMainVolatile) ;
+
+			BoolOfMainStatic = BoolOfMain;
+
+			MakeBtnGreen(sender);
+		}
+
+		private async void OnButton3_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			var checker = new StatusCheckerVolatile();
+			checker.StatusUpdated = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+			while (!checker.StatusUpdated) ;
+
+			BoolOfMainStatic = checker.StatusUpdated;
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton4_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			var checker = new StatusChecker();
+			checker.StatusUpdated = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+			while (!checker.StatusUpdated) ;
+
+			BoolOfMainStatic = checker.StatusUpdated;
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton5_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			var checker = new StatusCheckerVolatileRead();
+			//checker.StatusUpdated = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+			while (!checker.StatusUpdated) ;
+
+			BoolOfMainStatic = checker.StatusUpdated;
+
+			MakeBtnGreen(sender);
+		}
+
+		private async void OnButton6_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+
+			var checker = new StatusCheckerVolatileLock();
+			//checker.StatusUpdated = false;
+
+			//await checker.UpdateStatus().ConfigureAwait(false); // this one does not hang and sees the right value
+			Task www = checker.UpdateStatus(); // this one hangs forever
+											   //Task.Run(delegate { return checker.UpdateStatus(); }); // this one hangs forever
+
+			while (!checker.StatusUpdated) ;
+
+			BoolOfMainStatic = checker.StatusUpdated;
+
+			MakeBtnGreen(sender);
+		}
+		private async void OnButton7_Click(object sender, RoutedEventArgs e)
+		{
+			MakeBtnRed(sender);
+			// this fails sometimes, it's most interesting.
+			BoolOfMainStatic = false;
+			_persistent.Bool0 = false;
+			BoolOfMain = false;
+			BoolOfMainVolatile = false;
+			BoolOfMainVolatileReadWrite = false;
+			BoolOfMainVolatileRead = false;
+
+			Task away = Task.Run(async delegate
+		   {
+			   await Task.Delay(500).ConfigureAwait(false);
+
+			   BoolOfMainStatic = true;
+			   _persistent.Bool0 = true;
+			   BoolOfMain = true;
+			   BoolOfMainVolatile = true;
+			   BoolOfMainVolatileReadWrite = true;
+			   BoolOfMainVolatileRead = true;
+
+			   Debug.WriteLine("updates done");
+		   });
+
+			//while (!BoolOfMainStatic) ; // this works
+			//Debugger.Break();
+			//while (!_persistent.Bool0) ; // this works
+			//Debugger.Break();
+			while (!BoolOfMain) ; // this hangs forever; however, 
+								  // if true is READ after reading a volatile or static property, then it works, 
+								  // never mind if the volatile or static property is set before or after it.
+			// this cannot be reproduced 100%, except that it always hangs when BoolOfMain is written and read alone.
+			// if it is read first, it always hangs.
+
+			//Debugger.Break();
+			//while (!BoolOfMainVolatile) ; // this works
+			//Debugger.Break();
+			//while (!BoolOfMainVolatileReadWrite) ; // this works
+			//Debugger.Break();
+			while (!BoolOfMainVolatileRead) ; // this works
+
+			MakeBtnGreen(sender);
+		}
+
+
+		#region utilz
+		private void MakeBtnRed(object sender)
+		{
+			Task ss = RunInUiThreadAsync(delegate
+			{
+				(sender as Button).Foreground = new SolidColorBrush(Colors.Red);
+			});
+		}
+		private void MakeBtnGreen(object sender)
+		{
+			Task ss = RunInUiThreadAsync(delegate
+			{
+				(sender as Button).Foreground = new SolidColorBrush(Colors.Green);
+			});
+		}
+		private async Task Update_Bool0AndBoolStatic_ToTrue_Async()
+		{
+			await Task.Delay(500).ConfigureAwait(false);
+			BoolOfMainStatic = true;
+			_persistent.Bool0 = true;
+			BoolOfMain = true;
+		}
+
+		private void UpdateTbs()
+		{
+			Task upd = RunInUiThreadAsync(delegate
+			{
+				TB_Bool0.Text = _persistent.Bool0.ToString();
+				TB_Bool0Static.Text = BoolOfMainStatic.ToString();
+				TB_Bool1.Text = _persistent.Bool0.ToString();
+				TB_Bool1Static.Text = BoolOfMainStatic.ToString();
+				TB_Bool2.Text = _persistent.Bool0.ToString();
+				TB_Bool2Static.Text = BoolOfMainStatic.ToString();
+				TB_Bool3.Text = _persistent.Bool0.ToString();
+				TB_Bool3Static.Text = BoolOfMainStatic.ToString();
+			});
+		}
+
+		private void ResetValues()
+		{
+			BoolOfMainStatic = false; _persistent.Bool0 = false;
+		}
+
+		class StatusChecker
+		{
+			public string Result;
+			public bool StatusUpdated;
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				StatusUpdated = true;
+			}
+		}
+		class StatusCheckerVolatile
+		{
+			public string Result;
+			public volatile bool StatusUpdated;
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				StatusUpdated = true;
+			}
+		}
+		class StatusCheckerVolatileRead
+		{
+			public string Result;
+			private bool _statusUpdated = false;
+			public bool StatusUpdated { get { return Volatile.Read(ref _statusUpdated); } }
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				_statusUpdated = true;
+			}
+		}
+		class StatusCheckerVolatileLock
+		{
+			private readonly object _locker = new object();
+			public string Result;
+			private bool _statusUpdated = false;
+			public bool StatusUpdated
+			{
+				get
+				{
+					lock (_locker) // no locker will break it
+					{
+						return _statusUpdated;
+					}
+				}
+			}
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				_statusUpdated = true;
+			}
+		}
+		class StatusCheckerWithRefToMain
+		{
+			private MainPage _owner = null;
+			public string Result;
+
+			public StatusCheckerWithRefToMain(MainPage owner)
+			{
+				_owner = owner;
+			}
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				_owner.BoolOfMain = true;
+			}
+		}
+		class StatusCheckerWithRefToPersistent
+		{
+			private Persistent _owner = null;
+			public string Result;
+
+			public StatusCheckerWithRefToPersistent(Persistent owner)
+			{
+				_owner = owner;
+			}
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				_owner.Bool0 = true;
+			}
+		}
+		class StatusCheckerWithRef_VolatileOwner
+		{
+			private MainPage _owner = null;
+			public string Result;
+
+			public StatusCheckerWithRef_VolatileOwner(MainPage owner)
+			{
+				_owner = owner;
+			}
+
+			public async Task UpdateStatus()
+			{
+				await Task.Delay(2000).ConfigureAwait(false);
+				Result = "OK";
+				Debug.WriteLine("Status Obtained");
+				_owner.BoolOfMainVolatile = true;
+			}
+		}
+		#endregion utilz
+	}
+}
